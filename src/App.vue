91926c26797e50fb699a2d82c7fad75595e21083
jovandosen <jovandosen1994@gmail.com>
2020-06-10 10:24:36 +0200

    Navigation updated

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> 
-            <router-link to="/about">About</router-link>
-            <router-link to="/contact">Contact</router-link> 
-            <router-link to="/team">Team</router-link>
-            <router-link to="/gallery">Gallery</router-link>
-            <router-link to="/register" v-bind:class="auth">Register</router-link>
-            <router-link to="/login" v-bind:class="auth">Login</router-link>
-        </div> 
+        <TheNavigation /> 
         <router-view/>
     </div>
 </template>
 
 <script>
+    import TheNavigation from "@/components/TheNavigation"
     export default {
-        data() {
-            return {
-                auth: 'auth-links'
-            }
+        components: {
+            TheNavigation
         }
     }
 </script>
@@ -28,36 +19,6 @@
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
-
-#nav {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    font-size: 20px;
-    padding: 20px 0px 20px 20px;
-    background-color: #808080;
-}
-
-#nav a {
-    text-decoration: none;
-    color: white;
-    margin-right: 20px; 
-}
-
-#nav a.router-link-exact-active {
-    color: #80bfff;
-    border-bottom: 2px solid #80bfff;
-}
-
-#nav a:hover {
-    color: #b8b894;
-}
-
-#nav a:active {
-    color: #ff8080;
-}
-
-.auth-links {
-    float: right;
 }
 
 .container {
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="nav">
+        <ul>
+            <li v-for="link in links" v-bind:key="link.id" v-bind:class="link.class">
+                <router-link :to="{ name: link.name }">{{ link.name }}</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                { id: 1, slug: 'home', name: 'Home', class: '' },
+                { id: 2, slug: 'about', name: 'About', class: '' },
+                { id: 3, slug: 'contact', name: 'Contact', class: '' },
+                { id: 4, slug: 'team', name: 'Team', class: '' },
+                { id: 5, slug: 'gallery', name: 'Gallery', class: '' },
+                { id: 6, slug: 'register', name: 'Register', class: 'auth-links' },
+                { id: 7, slug: 'login', name: 'Login', class: 'auth-links' }
+            ]
+        }
+    }
+}    
+</script>
+
+<style scoped>
+#nav {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-size: 20px;
+    padding: 20px 0px 20px 20px;
+    background-color: #808080;
+}
+
+#nav ul {
+    list-style-type: none;
+}
+
+#nav ul li {
+    display: inline-block;
+}
+
+#nav ul li a {
+    text-decoration: none;
+    color: white;
+    margin-right: 20px; 
+}
+
+#nav ul li a.router-link-exact-active {
+    color: #80bfff;
+    border-bottom: 2px solid #80bfff;
+}
+
+#nav ul li a:hover {
+    color: #b8b894;
+}
+
+#nav ul li a:active {
+    color: #ff8080;
+}
+
+.auth-links {
+    float: right;
+}    
+</style>
